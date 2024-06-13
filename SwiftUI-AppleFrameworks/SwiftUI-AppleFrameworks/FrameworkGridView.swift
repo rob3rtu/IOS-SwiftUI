80ed1739ab904133e9d6c58a5b6fcc80622da9c7
Robert <robert.udrea@cst.ro>
2024-06-13 20:37:19 +0300

    second project init

---
+++ SwiftUI-AppleFrameworks/SwiftUI-AppleFrameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  SwiftUI-AppleFrameworks
+//
+//  Created by Robert  on 13.06.2024.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
--- SwiftUI-AppleFrameworks/SwiftUI-AppleFrameworks/SwiftUI_AppleFrameworksApp.swift
+++ SwiftUI-AppleFrameworks/SwiftUI-AppleFrameworks/SwiftUI_AppleFrameworksApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_AppleFrameworksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FrameworkGridView()
         }
     }
 }
