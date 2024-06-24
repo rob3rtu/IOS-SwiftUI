3b2057d8e0bf8f7a94787052ad659f234f7e6964
Robert <robert.udrea@cst.ro>
2024-06-24 17:29:21 +0300

    more fixes

--- Appetizers/Appetizers/Screens/AppetizerListCell.swift
+++ Appetizers/Appetizers/Screens/AppetizerListCell.swift
@@ -13,10 +13,25 @@ struct AppetizerListCell: View {
     
     var body: some View {
         HStack {
-            AppetizerRemoteImage(urlString: appetizer.imageURL)
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                .frame(width: 120, height: 90)
-                .clipShape(.rect(cornerRadius: 10))
+            //            AppetizerRemoteImage(urlString: appetizer.imageURL)
+            //                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            //                .frame(width: 120, height: 90)
+            //                .clipShape(.rect(cornerRadius: 10))
+            
+            AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 120, height: 90)
+                    .clipShape(.rect(cornerRadius: 10))
+            } placeholder: {
+                Image("food-placeholder")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 120, height: 90)
+                    .clipShape(.rect(cornerRadius: 10))
+            }
+            
             
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
--- Appetizers/Appetizers/Utilities/Extensions/String+Ext.swift
+++ Appetizers/Appetizers/Utilities/Extensions/String+Ext.swift
@@ -6,13 +6,44 @@
 //
 
 import Foundation
+import RegexBuilder
 
 extension String {
     
     var isValidEmail: Bool {
-        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
-        return emailPredicate.evaluate(with: self)
+//        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+//        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+//        return emailPredicate.evaluate(with: self)
+        
+        let emailRegex = Regex {
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("._%+-"),
+                    ("A"..."Z"),
+                    ("0"..."9"),
+                    ("a"..."z")
+                )
+            }
+            "@"
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("-"),
+                    ("A"..."Z"),
+                    ("a"..."z"),
+                    ("0"..."9")
+                )
+            }
+            "."
+            Repeat(2...64) {
+                CharacterClass(
+                    ("A"..."Z"),
+                    ("a"..."z")
+                )
+            }
+        }
+        
+        return self.wholeMatch(of: emailRegex) != nil
+        
         
     }
 }
