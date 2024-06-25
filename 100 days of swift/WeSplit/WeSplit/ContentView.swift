59713de80f0718f84dbb38068bd1bd2610251c8d
Robert <robert.udrea@cst.ro>
2024-06-25 14:44:09 +0300

    100 days of swift first proj init

---
+++ 100 days of swift/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Robert  on 25.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        Text("Hello there")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100 days of swift/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Robert  on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
