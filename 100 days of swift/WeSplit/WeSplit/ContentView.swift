f59a826ab26a7197da9f90be6257af3fb805c63c
Robert <robert.udrea@cst.ro>
2024-06-26 17:04:45 +0300

    was ist das

--- 100 days of swift/WeSplit/WeSplit/ContentView.swift
+++ 100 days of swift/WeSplit/WeSplit/ContentView.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var value = 3.4
     
     var body: some View {
-        Text("Hello there")
+        Form {
+            Section {
+                TextField("Amount", value: $value, format: .currency(code: "USD"))
+            }
+        }
     }
 }
 
